daf5afbffe9e3c46c53ac65aa20b6a966a4f7e58
Jovia Minallah Matata <joviaminallah@gmail.com>
2026-03-02 08:34:32 +0300

    Revert "Added testimonials, application pages"

--- frontend/scaleup/src/App.jsx
+++ frontend/scaleup/src/App.jsx
@@ -1,17 +1,9 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Testimonials from './pages/Testimonials'
-import ApplicationDetails from './pages/ApplicationDetails'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Testimonials />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-        <Route path="/join-us" element={<ApplicationDetails />} />
-      </Routes>
-    </Router>
+      <h1 className="text-blue-500">Hi</h1>
+ 
   )
 }
 
